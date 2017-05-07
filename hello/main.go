c1d6d69542f188c80710c761a7114983ba142b88
Chris Park <cpark@basistech.com>
2017-05-07 12:31:07 -0400

    Updates

--- hello/main.go
+++ hello/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Hello!")
+	fmt.Printf("%d \t %b \t %#x\n", 42, 42, 42)
+
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d\t%b\t%#x\t%q\n", i, i, i, i)
+	}
 }
